b24a8e2e7e9137c9996f96b9001c2c24b81a84bf
sgh1939 <rbghks19@naver.com>
2023-03-20 17:16:01 +0900

    프로젝트 개요 수정

---
+++ member-service/src/main/generated/com/reservation/member/application/mapper/SignUpRequestMapperImpl.java
@@ -0,0 +1,88 @@
+package com.reservation.member.application.mapper;
+
+import com.reservation.member.domain.Member;
+import com.reservation.member.dto.SignUpRequest;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-03-17T15:45:20+0900",
+    comments = "version: 1.5.2.Final, compiler: javac, environment: Java 17.0.5 (Oracle Corporation)"
+)
+@Component
+public class SignUpRequestMapperImpl implements SignUpRequestMapper {
+
+    @Override
+    public SignUpRequest toDto(Member entity) {
+        if ( entity == null ) {
+            return null;
+        }
+
+        SignUpRequest signUpRequest = new SignUpRequest();
+
+        return signUpRequest;
+    }
+
+    @Override
+    public Member toEntity(SignUpRequest dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        String userId = null;
+        String username = null;
+        String password = null;
+        String phoneNum = null;
+        String address = null;
+
+        userId = dto.getUserId();
+        username = dto.getUsername();
+        password = dto.getPassword();
+        phoneNum = dto.getPhoneNum();
+        address = dto.getAddress();
+
+        Long id = null;
+
+        Member member = new Member( id, userId, username, password, phoneNum, address );
+
+        return member;
+    }
+
+    @Override
+    public List<SignUpRequest> toDto(List<Member> e) {
+        if ( e == null ) {
+            return null;
+        }
+
+        List<SignUpRequest> list = new ArrayList<SignUpRequest>( e.size() );
+        for ( Member member : e ) {
+            list.add( toDto( member ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public List<Member> toEntity(List<SignUpRequest> d) {
+        if ( d == null ) {
+            return null;
+        }
+
+        List<Member> list = new ArrayList<Member>( d.size() );
+        for ( SignUpRequest signUpRequest : d ) {
+            list.add( toEntity( signUpRequest ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public void updateFromDto(SignUpRequest dto, Member entity) {
+        if ( dto == null ) {
+            return;
+        }
+    }
+}
