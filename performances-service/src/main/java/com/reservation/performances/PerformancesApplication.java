65394064d50bc1762c1e131d53a63a54674509a7
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2023-03-29 23:05:59 +0900

    init : 공연 서비스 초기화

---
+++ performances-service/src/main/java/com/reservation/performances/PerformancesApplication.java
@@ -0,0 +1,13 @@
+package com.reservation.performances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformancesApplication.class, args);
+	}
+
+}
