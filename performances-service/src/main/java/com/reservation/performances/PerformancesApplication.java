45cfe7cf16edc13be2a2fed15f33915e4ab35ee9
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2023-03-31 18:06:27 +0900

    .

--- performances-service/src/main/java/com/reservation/performances/PerformancesApplication.java
+++ performances-service/src/main/java/com/reservation/performances/PerformancesApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PerformancesApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PerformancesApplication.class, args);
 	}
