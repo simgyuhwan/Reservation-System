c59fe49c042a6e4a3b598c74767038fdc66c21d9
sgh1939 <rbghks19@naver.com>
2023-04-26 16:21:54 +0900

    feat : 공연 예약 신청 api 성공 테스트 구현

--- reservation-parent/reservationservice/src/main/java/com/sim/reservationservice/api/ReservationController.java
+++ reservation-parent/reservationservice/src/main/java/com/sim/reservationservice/api/ReservationController.java
@@ -1,7 +1,16 @@
 package com.sim.reservationservice.api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sim.reservationservice.application.ReservationCommandService;
+import com.sim.reservationservice.dto.request.ReservationDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,9 +22,16 @@ import lombok.RequiredArgsConstructor;
  * @since 2023.04.25
  */
 @RestController
-@RequestMapping("/api/reservations")
+@RequestMapping("/api/performances")
 @RequiredArgsConstructor
 public class ReservationController {
+	private final ReservationCommandService reservationCommandService;
 
+	@PostMapping("/{performanceId}/schedules/{scheduleId}/reservations")
+	public ResponseEntity reservation(@PathVariable Long performanceId, @PathVariable Long scheduleId,
+		@RequestBody @Validated ReservationDto reservationDto) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(
+			reservationCommandService.createReservation(performanceId, scheduleId, reservationDto));
+	}
 
 }
