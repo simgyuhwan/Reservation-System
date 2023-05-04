9ec961a4d2157d8a7c7681df948a7664634d1f64
sgh1939 <rbghks19@naver.com>
2023-05-04 15:29:01 +0900

    config : 회원 서비스 FeignClient 등록

---
+++ reservation-parent/memberservice/src/main/java/com/reservation/memberservice/client/PerformanceApiClient.java
@@ -0,0 +1,23 @@
+package com.reservation.memberservice.client;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.reservation.common.dto.PerformanceDto;
+
+/**
+ * PerformanceApiClient.java
+ * Performance Service에 요청 api 클래스
+ *
+ * @author sgh
+ * @since 2023.05.04
+ */
+@FeignClient(name = "performance")
+public interface PerformanceApiClient {
+
+	@GetMapping
+	List<PerformanceDto> getPerformanceByUserId(@RequestParam("userId") String userId);
+}
