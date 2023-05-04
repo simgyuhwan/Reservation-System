9f8ae38ef80504e6d2d5eea70cd036d091bdca45
sgh1939 <rbghks19@naver.com>
2023-05-04 10:39:53 +0900

    module : 이벤트 서비스 추가

---
+++ reservation-parent/eventservice/src/test/java/com/reservation/eventservice/EventserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservation.eventservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
