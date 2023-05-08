bbf97a9933589c98a7cb66f49996e46f0cfc40f0
sgh1939 <rbghks19@naver.com>
2023-05-08 17:55:06 +0900

    refactor : eventType name 변경

--- reservation-parent/performanceservice/src/main/java/com/reservation/performanceservice/types/EventType.java
+++ reservation-parent/performanceservice/src/main/java/com/reservation/performanceservice/types/EventType.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum EventType {
-	PERFORMANCE_CREATED("Performance Created Event");
+	PERFORMANCE_CREATED("PerformanceCreatedEvent");
 
 	private final String name;
 
