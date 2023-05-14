98d7646ca04670f5277276b51afea1bf75a82db9
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2023-05-14 15:08:01 +0900

    refactor : 회원 서비스 모듈화 분리 완료

--- member/app/api/src/main/java/com/sim/member/dto/request/MemberUpdateRequest.java
+++ member/app/api/src/main/java/com/sim/member/dto/request/MemberUpdateRequest.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
