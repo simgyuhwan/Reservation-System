93c4ecf6c3ee9755a895c42b2a4548e74d61b419
sgh1939 <rbghks19@naver.com>
2023-05-15 10:13:30 +0900

    refactor : Reservation Service 모듈화 진행 - 구조 설계

---
+++ reservation/boot/boot-api/src/main/java/com/sim/reservation/boot/api/BootApiApplication.java
@@ -0,0 +1,13 @@
+package com.sim.reservation.boot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootApiApplication.class, args);
+	}
+
+}
---
+++ reservation/data/data-reservation/src/main/java/com/sim/reservation/data/reservation/DataReservationApplication.java
@@ -0,0 +1,13 @@
+package com.sim.reservation.data.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataReservationApplication.class, args);
+	}
+
+}
