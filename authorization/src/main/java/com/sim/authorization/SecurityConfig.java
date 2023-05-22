5d0c1269afd01d014aad086f8907f18b240a4972
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2023-05-22 23:10:54 +0900

    config : 인증 서버 초기 구성

---
+++ authorization/src/main/java/com/sim/authorization/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.sim.authorization;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+}
