a63c6847ee0f91ec55c7dbacec878548eb0c4f82
simgyuhwan <rbghks19@naver.com>
2023-05-23 16:55:03 +0900

    config : event service 오케스트레이션 방식으로 모듈 초기 구성

---
+++ event/event-orchestration/src/main/java/com/sim/event/orchestration/EventOrchestrationApplication.java
@@ -0,0 +1,13 @@
+package com.sim.event.orchestration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventOrchestrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventOrchestrationApplication.class, args);
+	}
+
+}
---
+++ event/notification-service/src/main/java/com/sim/event/notificationservice/NotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sim.event.notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationServiceApplication.class, args);
+	}
+
+}
