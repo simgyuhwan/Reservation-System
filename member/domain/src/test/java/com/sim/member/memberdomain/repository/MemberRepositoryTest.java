febe3d266c07bf71e68e4f53a2ece3991d6df965
simgyuhwan <rbghks19@naver.com>
2023-08-17 16:43:54 +0900

    refactor : MemberRepositoryTest 1. 사용하지 않는 EntityManager 제거

--- member/domain/src/test/java/com/sim/member/memberdomain/repository/MemberRepositoryTest.java
+++ member/domain/src/test/java/com/sim/member/memberdomain/repository/MemberRepositoryTest.java
@@ -1,21 +1,18 @@
 package com.sim.member.memberdomain.repository;
 
-import static org.assertj.core.api.Assertions.*;
-
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Optional;
-
+import com.sim.member.memberdomain.domain.Member;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.DataIntegrityViolationException;
 
-import com.sim.member.memberdomain.domain.Member;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
  * MemberRepositoryTest.java
