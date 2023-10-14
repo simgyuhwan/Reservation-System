b69728e10eedcd5077dfaf1a1bafd30c280026a7
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2023-10-14 22:03:15 +0900

    refactor : 분산락 annotation, aop 추가

--- reservation/data/data-reservation/src/main/generated/com/sim/reservation/data/reservation/mapper/PerformanceInfoMapperImpl.java
+++ reservation/data/data-reservation/src/main/generated/com/sim/reservation/data/reservation/mapper/PerformanceInfoMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-09-12T14:10:19+0900",
+    date = "2023-09-12T14:58:38+0900",
     comments = "version: 1.5.2.Final, compiler: javac, environment: Java 17.0.6 (Amazon.com Inc.)"
 )
 @Component
---
+++ reservation/data/data-reservation/src/main/java/com/sim/reservation/data/reservation/util/CustomSpringELParser.java
@@ -0,0 +1,24 @@
+package com.sim.reservation.data.reservation.util;
+
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+
+/**
+ * Created by Gyuhwan
+ */
+public class CustomSpringELParser {
+
+    public CustomSpringELParser() {
+    }
+
+    public static Object getDynamicValue(String[] parameterNames, Object[] args, String key) {
+        ExpressionParser parser = new SpelExpressionParser();
+        StandardEvaluationContext context = new StandardEvaluationContext();
+
+        for(int i=0; i< parameterNames.length; i++) {
+            context.setVariable(parameterNames[i], args[i]);
+        }
+        return parser.parseExpression(key).getValue(context, Object.class);
+    }
+}
